8ef7348db296be733e13e840bce4d3ef8ca900c7
txb <txb@yazoteach.com>
2012-03-31 17:34:27 +0800

    check

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -1,51 +1,39 @@
 module Admin
   class ImportsController < AdminController
   	def index
-  	  @balances = Balance.where('status=0')
-  	end
-    def checkafter
-      @balances = Balance.where('status=0')
-      render 'checkafter.html.erb'
+     @balances = Balance.new_input
     end
+  def checkdate
+    Balance.new_input.each do |b|
+      ch = Channel.where(:channel_number => b.channel_number).first
+      if ch
+        b.product_id = ch.product_id
+        b.client_id = ch.client_id
 
-def checkdate
-        bal=Balance.where('status=0')
-    if bal != nil
-        bal.each do |b|
-               ch=Channel.where(:channel_number => b.channel_number)
-                if ch != nil
-                    ch.each do |c|
-                         agr=Agreement.where(:product_id => c.product_id)
-                             if agr != nil
-                                  agr.each do |a|
-                                     b.price = a.price
-                                     b.product_id = c.product_id
-                                     b.client_id = c.client_id
-                                     b.company_id = a.company_id
-                                     b.agreement_id = a.id
-                                     b.amount = b.count*a.price
-                                     b.save!
-                                   end
-                               end
-                    end
-               end
+        agr = Agreement.where(['client_id=? and product_id=? and agreement_start>=? and agreement_end<=?', ch.client_id, ch.product_id, b.balance_date, b.balance_date]).first
+        if agr
+          b.company_id = agr.comapny_id
+          b.agreement_id = agr.id
+          b.price = agr.price
+          b.amount = b.count * b.price
         end
-      redirect_to :action => :checkafter, :controller => :imports
+        b.save
+      end
     end
-end
-
-
-def save
-   Balance.where('status=0').each do |b|
-          ch = Channel.find_by_id(b.channel_number)
-          if ch
-            b.company_id= ch.company_id
-            b.status = 1
-            b.save!
-          end
-        end
     redirect_to :action => :index, :controller => :imports
-end
+  end
+
+  def save
+   Balance.where('status=0').each do |b|
+    ch = Channel.find_by_id(b.channel_number)
+    if ch
+      b.company_id= ch.company_id
+      b.status = 1
+      b.save!
+    end
+  end
+  redirect_to :action => :index, :controller => :imports
+  end
 
 def clear
   Balance.where('status=0').delete_all
@@ -53,33 +41,33 @@ def clear
 end
 
 def show
-      if params[:op] == 'clear'
-        Balance.where('status=0').delete_all
-      elsif params[:op] == 'import'
-        data = params[:import_text]
-        lines = data.split(/\n/)
-        lines.each do |line|
-          cs = line.split(/[\t\s]/)
-          if cs.length>2
-            b = Balance.new
-            b.balance_date = cs[0]
-            b.channel_number = cs[1]
-            b.count = cs[2]
-            b.status = 0
-            b.save!
-          end
-        end
-      elsif params[:op] == 'save'
-        Balance.where('status=0').each do |b|
-          ch = Channel.find_by_id(b.channel_number)
-          if ch
-            b.company_id = ch.company_id
-            b.status = 1
-            b.save!
-          end
-        end
+  if params[:op] == 'clear'
+    Balance.where('status=0').delete_all
+  elsif params[:op] == 'import'
+    data = params[:import_text]
+    lines = data.split(/\n/)
+    lines.each do |line|
+      cs = line.split(/[\t\s]/)
+      if cs.length>2
+        b = Balance.new
+        b.balance_date = cs[0]
+        b.channel_number = cs[1]
+        b.count = cs[2]
+        b.status = 0
+        b.save!
       end
-      redirect_to :action => :index, :controller => :imports
+    end
+  elsif params[:op] == 'save'
+    Balance.where('status=0').each do |b|
+      ch = Channel.find_by_id(b.channel_number)
+      if ch
+        b.company_id = ch.company_id
+        b.status = 1
+        b.save!
+      end
     end
   end
+  redirect_to :action => :index, :controller => :imports
+end
+end
 end
--- app/models/balance.rb
+++ app/models/balance.rb
@@ -3,4 +3,6 @@ class Balance < ActiveRecord::Base
 	belongs_to :product
 	belongs_to :client
 	belongs_to :agreement
+
+	scope :new_input, where(:status => 0)
 end
