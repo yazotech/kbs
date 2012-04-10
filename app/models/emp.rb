885b1a8c3769ec8d507bb931d5baadb05d1ff78e
txb <txb@yazoteach.com>
2012-04-10 10:30:11 +0800

    ee

--- app/models/emp.rb
+++ app/models/emp.rb
@@ -3,7 +3,7 @@ class Emp < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable, :encryptable, :confirmable, :lockable
+         :token_authenticatable, :encryptable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login_name, :email, :password, :password_confirmation, :remember_me
