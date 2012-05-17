20cf69839f005516e32cf40d6bc79eacd1a31a05
Jimmy <lyh@yazotech.com>
2012-05-17 16:09:52 +0800

    bugs and icons

---
+++ app/assets/javascripts/admin/all.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require admin/dialog
+
+//= require jquery.inc/metadata
+//= require jquery.mb.menu/mbMenu
+//= require jquery.inc/styleswitch
+//= require jquery.inc/hoverIntent
+//= require admin/menu
