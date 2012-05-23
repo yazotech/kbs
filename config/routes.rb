b5933b94220aa6e2ff40b60601acc7a44c4b6433
txb <txb@yazoteach.com>
2012-05-23 17:00:33 +0800

    baobiao

--- config/routes.rb
+++ config/routes.rb
@@ -23,12 +23,14 @@ Kbs::Application.routes.draw do
       end
     end
     match 'checkpro' => 'channels#checkpro'
-
+    match 'mm_data' => 'reports#mm_data'
     match 'sjflot' => 'imports#sjfloat'
-
+    match 'wap_read' => 'reports#wap_read'
     match 'data_tools(/:action)' => 'data_tools'
     match 'reports(/:action)' => 'reports'
     match 'diagrams(/:action)' => 'diagrams'
+    match 'yz_caixin' => 'reports#yz_caixin'
+    match 'zy_caixin' => 'reports#zy_caixin'
   end
   match '/admin', :to => 'admin/home#index', :as => :admin
 
